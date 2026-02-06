038a03d4b274d76f8186fc10312a8d2ac330f613
PedroLucasCM <pcassianomartins@gmail.com>
2026-02-05 21:05:36 -0300

    Search Bar e Home

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,16 @@
 import { Image, ImageBackground, Text, View } from "react-native";
 
+import React from "react";
+import { Tabs } from "expo-router";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Tabs } from "expo-router";
-import React from "react";
 
 const TabIcon = ({ focused, icon, title }: any) => {
   if (focused) {
     return (
       <ImageBackground
         source={images.highlight}
-        className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
+        className="flex flex-row w-full flex-1 min-w-[114px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
       >
         <Image source={icon} tintColor="#151312" className="size-5" />
         <Text className="text-secondary text-base font-semibold ml-2">
