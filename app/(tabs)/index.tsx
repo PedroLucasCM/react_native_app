3383c2633657070b5aa25266310dba445064eb78
PedroLucasCM <pcassianomartins@gmail.com>
2026-02-07 17:34:49 -0300

    Hook, Fetch e api

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,27 @@
-import { Image, ScrollView, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 
 import SearchBar from "@/components/SearchBar";
+import { fetchMovies } from "@/services/api";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
 
 export default function Index() {
   const rounter = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }));
 
   return (
     <View className="flex-1 bg-primary">
@@ -19,12 +34,52 @@ export default function Index() {
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-        <View className="flex-1 mt-5">
-          <SearchBar
-            onPress={() => rounter.push("/search")}
-            placeholder="Search for a movie or show"
+        {moviesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#fff"
+            className="mt-10 self-center"
           />
-        </View>
+        ) : moviesError ? (
+          <Text>Error: {moviesError.message}</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => rounter.push("/search")}
+              placeholder="Search for a movie or show"
+            />
+
+            <>
+              <Text className="text-white text-center font-bold mt-5 mb-3">
+                Latest Movies!
+              </Text>
+
+              <FlatList
+                data={movies}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                  <View className="bg-secondary rounded-lg p-3 mb-3">
+                    <Image
+                      source={{
+                        uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+                      }}
+                      className="w-24 h-36 rounded-md mb-2"
+                    />
+                    <Text className="text-white font-bold">{item.title}</Text>
+                  </View>
+                )}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: "space-between",
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10,
+                }}
+                showsVerticalScrollIndicator={false}
+              />
+            </>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -8,7 +8,7 @@ interface Props {
   onPress?: () => void;
 }
 
-const SearchBar = ({ placeholder, onPress }: Props) => {
+function SearchBar({ placeholder, onPress }: Props) {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
       <Image
@@ -27,6 +27,6 @@ const SearchBar = ({ placeholder, onPress }: Props) => {
       />
     </View>
   );
-};
+}
 
 export default SearchBar;
